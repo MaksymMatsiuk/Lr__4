c1182f3d8bac3d8900343ff26bafdc4fa9d78c75
Maksym <maksim.matsuik567@gmail.com>
2024-03-03 18:52:58 +0200

    add greatest common denominator

--- test_.py
+++ test_.py
@@ -19,5 +19,20 @@ class TestCalculator(unittest.TestCase):
         result = calc.exp()
         self.assertEqual(result, -0.125)
 
+    def test_gcd_positive_numbers(self):
+        calc = Calculator(54, 24)
+        result = calc.gcd()
+        self.assertEqual(result, 6)
+
+    def test_gcd_negative_numbers(self):
+        calc = Calculator(-54, 24)
+        result = calc.gcd()
+        self.assertEqual(result, 6)
+
+    def test_gcd_mixed_numbers(self):
+        calc = Calculator(-54, -24)
+        result = calc.gcd()
+        self.assertEqual(result, 6)
+
 if __name__ == '__main__':
     unittest.main()
