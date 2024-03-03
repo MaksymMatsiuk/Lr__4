5a21d813bbfff52127e334dca1ddb15429f52181
Maksym <maksim.matsuik567@gmail.com>
2024-03-03 18:39:05 +0200

    add exponentiation operation

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 class Calculator():
-        def __init__(self,a,b):
-                self.a=a
-                self.b=b
-        def add(self):
-                return  self.a + self.b
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def exp(self):
+        return  self.a ** self.b
---
+++ test_.py
@@ -0,0 +1,23 @@
+import unittest
+from calc import Calculator
+
+class TestCalculator(unittest.TestCase):
+
+
+    def test_exponentiation_positive_numbers(self):
+        calc = Calculator(2, 3)
+        result = calc.exp()
+        self.assertEqual(result, 8)
+
+    def test_exponentiation_negative_numbers(self):
+        calc = Calculator(-2, 3)
+        result = calc.exp()
+        self.assertEqual(result, -8)
+
+    def test_exponentiation_mixed_numbers(self):
+        calc = Calculator(-2, -3)
+        result = calc.exp()
+        self.assertEqual(result, -0.125)
+
+if __name__ == '__main__':
+    unittest.main()
